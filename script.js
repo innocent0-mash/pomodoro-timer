126667508645299a7f65713a5cfccc0b247fdb2d
innocent0-mash <195310469+innocent0-mash@users.noreply.github.com>
2026-07-25 19:43:00 +0530

    feat: update timer settings and add mode selection functionality
    - different modes (study, gym, focus).
    - Improved CSS styles for better responsiveness and visual appeal.

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
-const WORK_TIME = 30 * 60;
-const SHORT_BREAK = 5 * 60;
-const LONG_BREAK =  30* 60;
-
+let WORK_TIME = 30 * 60;
+let SHORT_BREAK = 5 * 60;
+let LONG_BREAK = 25 * 60;
 let COUNTER = 0;
 let interval = null;
 let runing = false;
@@ -13,7 +12,10 @@ let timer = document.getElementById("timer");
 let btn = document.getElementById("startBtn");
 let reset = document.getElementById("reset");
 let skip = document.getElementById("skip");
-
+let mode = document.getElementById("sessionLabel");
+let study = document.getElementById("study");
+let gym = document.getElementById("gym");
+let focus = document.getElementById("focus");
 
 let worktime = () => {
     clearInterval(interval);
@@ -36,6 +38,31 @@ let longBreak = () => {
     time = LONG_BREAK;
     updateTimer();
 }
+
+let stud = () => {
+    WORK_TIME = 50 * 60;
+    SHORT_BREAK = 10 * 60;
+    LONG_BREAK = 25 * 60;
+    time = WORK_TIME;
+    updateTimer();
+}
+let gy = () => {
+    WORK_TIME = 30 * 60;
+    SHORT_BREAK = 5 * 60;
+    LONG_BREAK = 20 * 60;
+    time = WORK_TIME;
+
+    updateTimer();
+
+}
+let fcus = () => {
+    WORK_TIME = 60 * 60;
+    SHORT_BREAK = 10 * 60;
+    LONG_BREAK = 30 * 60;
+    time = WORK_TIME;
+    updateTimer();
+
+}
 function updateTimer() {
     let minutes = Math.floor(time / 60);
     let second = time % 60;
@@ -52,11 +79,14 @@ function switchMode() {
         COUNTER++;
         if (COUNTER >= 5) {
             longBreak();
+            // mode.innerText = "Relax Time";
         } else {
             shortBreak();
+            // mode.innerText = "Break Time";
         }
-    }else{
+    } else {
         worktime();
+        // mode.innerText = "Foucs Time"
     }
 };
 
@@ -113,3 +143,6 @@ skip.addEventListener("click", () => {
     updateTimer();
 });
 
+study.addEventListener("click", stud);
+gym.addEventListener("click", gy);
+focus.addEventListener("click", fcus);
